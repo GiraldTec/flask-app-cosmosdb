e8daa84fa7a752beacce66c1c7b92ceb54dc9db8
Neil Peterson <neilpeterson@outlook.com>
2019-08-12 13:57:15 -0700

    New Tests

--- azure-vote/azure-vote/tests/test_vote.py
+++ azure-vote/azure-vote/tests/test_vote.py
@@ -1,9 +1,25 @@
 import pytest
 import math
 
-def test_sqrt():
+def vote_one():
    num = 25
    assert math.sqrt(num) == 5
 
-def testsquare():
+def vote_two():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def failed_vote():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def connect_source():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def transact_perf():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def code_coverage():
    assert 2*20 == 40
