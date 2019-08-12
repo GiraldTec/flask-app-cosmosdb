9023da07ce437e30a1cd9d1629ceeaf73464ac3e
Neil Peterson <neilpeterson@outlook.com>
2019-08-12 14:44:10 -0700

    Test Names

--- azure-vote/azure-vote/tests/test_vote.py
+++ azure-vote/azure-vote/tests/test_vote.py
@@ -5,5 +5,12 @@ def test_vote1():
    num = 25
    assert math.sqrt(num) == 5
 
-def testsquare():
+def test_vote2():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def test_lint():
+   assert 2*20 == 40
+
+def test_coverage():
    assert 2*20 == 40
